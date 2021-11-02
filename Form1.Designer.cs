002bc1bf12e850184188a65e13cb4652c7b66518
Гордиевич Наталья Валерьевна <gordievich_nv@peleng.jsc.local>
2021-11-02 11:10:46 +0300

    this part defines avalaible collimators and displays to list

---
+++ Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace PK_PPU
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.buttonPower = new System.Windows.Forms.Button();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.comboBoxCollimators = new System.Windows.Forms.ComboBox();
+            this.label = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 115200;
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // buttonPower
+            // 
+            this.buttonPower.Location = new System.Drawing.Point(305, 190);
+            this.buttonPower.Name = "buttonPower";
+            this.buttonPower.Size = new System.Drawing.Size(99, 45);
+            this.buttonPower.TabIndex = 0;
+            this.buttonPower.Text = "Старт / Стоп";
+            this.buttonPower.UseVisualStyleBackColor = true;
+            this.buttonPower.Click += new System.EventHandler(this.buttonPower_Click);
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(206, 190);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(75, 45);
+            this.buttonSend.TabIndex = 0;
+            this.buttonSend.Text = "Отправить";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // comboBoxCollimators
+            // 
+            this.comboBoxCollimators.FormattingEnabled = true;
+            this.comboBoxCollimators.Location = new System.Drawing.Point(51, 214);
+            this.comboBoxCollimators.Name = "comboBoxCollimators";
+            this.comboBoxCollimators.Size = new System.Drawing.Size(104, 21);
+            this.comboBoxCollimators.TabIndex = 1;
+            this.comboBoxCollimators.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(72, 190);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(69, 13);
+            this.label.TabIndex = 2;
+            this.label.Text = "Коллиматор";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 124);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 251);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.comboBoxCollimators);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.buttonPower);
+            this.Name = "Form1";
+            this.Text = "7872.11.00.400";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button buttonPower;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.ComboBox comboBoxCollimators;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+
+namespace PK_PPU
+{
+    
+    public partial class Form1 : Form
+    {
+        const byte STARTBYTE = 170;
+        List<string> list = new List<string>();
+        byte[] bufTx = new byte[14];
+        byte[] bufRx = new byte[14];        
+        int countRx = 0;
+        int count_bytes = 0;
+        byte bt = 0;
+        public static bool endRX = false;
+        string[] ports;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ports = SerialPort.GetPortNames();
+           
+        }
+
+        public string send(byte x)
+        {
+            for(int i = 0; i < ports.Length; i++)
+            {
+                serialPort1.PortName = ports[i];
+                serialPort1.Open();
+
+                if(serialPort1.IsOpen)
+                {
+                    bufTx[0] = STARTBYTE;
+                    bufTx[1] = x;
+                    bufTx[13] = calcSumXOR(bufTx, 13);
+                    serialPort1.Write(bufTx, 0, 14);
+
+                    Thread.Sleep(100);
+                    if (endRX)
+                    {
+                        endRX = false;
+                        Console.WriteLine("Collimator is found: " + ports[i]);
+                        serialPort1.Close();
+                        return "collimator " + x + " " + ports[i];
+                    }
+                    else Console.WriteLine("COM is invalid: " + ports[i]);
+                    serialPort1.Close();
+
+                }
+            }
+            return "none";
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            endRX = false;
+            for(int i = 0; i < 14; i++)
+            {
+                bufRx[i] = 0;
+            }
+            while (serialPort1.BytesToRead > 0)
+            {
+                try
+                {
+                    bufRx[count_bytes] = (byte)serialPort1.ReadByte();
+                    count_bytes++;
+                    switch (count_bytes)
+                    {
+                        case 1: if (bufRx[0] != 170) count_bytes = 0; break;
+                        case 14: count_bytes = 0;
+                            if (calcSumXOR(bufRx, 14) == 0)
+                            {
+                                endRX = true;
+                                string result = "";
+                                if (bufRx[1] % 2 == 0) result = "ТВ" + bufRx[1] / 2;
+                                else result = result = "ТПВ" + bufRx[1] / 2;
+                                list.Add(result);
+                                Console.WriteLine("END RX: " + result); 
+                            }
+                            break;
+                    }
+                }
+                catch (Exception err)
+                {
+                    MessageBox.Show(err.Message);
+                }
+
+                if (endRX == true) break;               
+         
+            }
+            count_bytes = 0;
+            
+            //this.Invoke(new EventHandler(ShowData));
+        }
+
+       /* private void ShowData(object sender, EventArgs e)
+        {
+            label1.Text = Convert.ToString(countRx);
+            if (bufRx[1] % 2 == 0) list.Add("ТВ" + bufRx[1] / 2);
+            else list.Add("ТПВ" + bufRx[1] / 2);
+
+        }*/
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private byte calcSumXOR(byte[] bytes, byte length)
+        {
+            byte sum = 0;
+            for (int i = 0; i < length; i++)
+                sum ^= bytes[i];
+            return sum;
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            //label1.Text = Convert.ToString("ffff" + bt);          
+            
+        }
+
+        private void buttonPower_Click(object sender, EventArgs e)
+        {
+            string[] array;
+            for(byte i = 0; i < 8; i++)
+            {
+                string temp = send(i);
+                //if(!temp.Equals("none"))
+                //{
+                    //list.Add(temp);
+               // }
+            }
+
+            array = new string[list.Count];
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(list[i]);
+                array[i] = list[i];
+            }
+
+            comboBoxCollimators.Items.AddRange(array);
+            list.Clear();
+        }
+    }
+}
